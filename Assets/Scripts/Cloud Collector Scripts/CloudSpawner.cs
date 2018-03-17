49700bdd9c66777dcb951a8c145e52222f80ef85
Arjun Dass <ad.sam007@gmail.com>
2018-03-17 16:18:25 -0400

    Added Clouds collector and spawner scripts

---
+++ Assets/Scripts/Cloud Collector Scripts/CloudSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] clouds;
+
+	private float distanceBetweenClouds = 3f;
+	private float minX, maxX;
+	private float lastCloudPositionY;
+	private float controlX;
+
+
+	[SerializedField]
+	private GameObject[] collectables;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
