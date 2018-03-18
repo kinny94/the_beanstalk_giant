9b5cb719c127ad20bd453a7985393398e37a3189
Arjun Dass <ad.sam007@gmail.com>
2018-03-17 20:49:59 -0400

    Fixed the position of the player

--- Assets/Scripts/Cloud Collector Scripts/CloudSpawner.cs
+++ Assets/Scripts/Cloud Collector Scripts/CloudSpawner.cs
@@ -17,15 +17,16 @@ public class CloudSpawner : MonoBehaviour {
 	private GameObject[] collectables;
 	private GameObject player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
 	void Awake(){
 		controlX = 0;
 		SetMinAndMaxX ();
 		CreateClouds ();
+		player = GameObject.Find ("Player");
+	}
+
+	// Use this for initialization
+	void Start () {
+		PositionThePlayer ();
 	}
 
 	void SetMinAndMaxX(){
@@ -69,5 +70,34 @@ public class CloudSpawner : MonoBehaviour {
 			clouds [i].transform.position = temp;
 			positionY -= distanceBetweenClouds;	
 		}
+	}
+
+	void PositionThePlayer(){
+
+		GameObject[] darkClouds = GameObject.FindGameObjectsWithTag ("Deadly");
+		GameObject[] cloudsInGame = GameObject.FindGameObjectsWithTag ("Cloud");
+
+		for (int i = 0; i < darkClouds.Length; i++) {
+			if (darkClouds [i].transform.position.y == 0f) {
+				Vector3 t = darkClouds [i].transform.position;
+
+				// not having first cloud as the dark cloud
+				darkClouds [i].transform.position = new Vector3 (cloudsInGame [0].transform.position.x, cloudsInGame [0].transform.position.y,
+					cloudsInGame [0].transform.position.z);
+
+				cloudsInGame[0].transform.position = t;
+			}
+		}
+
+		Vector3 temp = cloudsInGame [0].transform.position;
+
+		for( int i=1; i<cloudsInGame.Length; i++){
+			if (temp.y < cloudsInGame [i].transform.position.y) {
+				temp = cloudsInGame[i].transform.position;
+			}
+		}
+
+		temp.y += 0.8f;
+		player.transform.position = temp;
 	}
 }
