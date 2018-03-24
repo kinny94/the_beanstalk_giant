f7f3c138fa13686c9c62c5b444d8f5201235e663
Arjun Dass <ad.sam007@gmail.com>
2018-03-24 16:10:10 -0400

    Added pause resume and quit functionality

---
+++ Assets/Scripts/Game Controllers/GameplayController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class GameplayController : MonoBehaviour {
+
+	public static GameplayController instance;
+
+	[SerializeField]
+	private Text scoreText, coinText, lifeText;
+
+	[SerializeField]
+	private GameObject pausePanel;
+
+	void Awake(){
+		makeInstance ();
+	}
+	
+	void makeInstance(){
+
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	public void pauseTheGame(){
+		Time.timeScale = 0;
+		pausePanel.SetActive (true);
+	}
+
+	public void ResumeGame(){
+		Time.timeScale = 1f;
+		pausePanel.SetActive (false);
+	}
+
+	public void QuitGame(){
+		Time.timeScale = 1f;
+		Application.LoadLevel ("MainMenu");
+	}
+}
--- Assets/Scripts/Game Controllers/HighScoreController.cs
+++ Assets/Scripts/Game Controllers/HighScoreController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class HighScoreController : MonoBehaviour {
 
+	private GameplayController gc;
 	// Use this for initialization
 	void Start () {
 		
