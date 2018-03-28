fd606f638d66800a5a26da4f7adaeada6b36b174
Arjun Dass <anaraya1@stevens.edu>
2018-03-27 21:54:21 -0400

    added gamemanager

---
+++ Assets/Scripts/Game Controllers/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	[HideInInspector]
+	public bool gameStartedFromMainMenu, gameRestartAfterPlayerDied;
+
+	[HideInInspector]
+	public int score, coinScore, lifeScore;
+
+	void Awake () {
+		MakeSingleton ();
+	}
+
+	void OnLevelWasLoaded(){
+		if (Application.loadedLevelName == "Gameplay") {
+			if (gameRestartedAfterPlayerDied) {
+				GameplayController.instance.setScore (score);
+				GameplayController.instance.setCoinScore (coinScore);
+				GameplayController.instance.setLifeScore (lifeScore);
+
+				PlayerScript.scoreCount = score;
+				PlayerScore.coinScore = coinScore; 
+				PlayerScore.lifeScore = lifeScore;
+			}
+		}
+	}
+
+	void MakeSingleton () {
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		}
+	}
+}
